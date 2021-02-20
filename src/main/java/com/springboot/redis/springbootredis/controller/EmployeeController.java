db1988bff7f3325d401907e7fa38755b061da8f1
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-02-20 19:06:45 +0300

    Task 25 : Fixed some code in findById of EmployeeController

--- src/main/java/com/springboot/redis/springbootredis/controller/EmployeeController.java
+++ src/main/java/com/springboot/redis/springbootredis/controller/EmployeeController.java
@@ -27,7 +27,7 @@ public class EmployeeController {
         return employeeService.findAll();
     }
 
-    @Cacheable(value = "employees", key = "#id", unless = "#result.get().phone.equals('111-222-3333')")
+    @Cacheable(value = "employees", key = "#id", unless = "#result.get().phone.equals('111-222-4444')")
     @GetMapping("/find/{id}")
     public Optional<Employee> findById(@PathVariable Long id) {
         LOG.info("Getting Employee with ID {}.", id);
