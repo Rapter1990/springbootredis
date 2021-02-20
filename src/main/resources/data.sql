fa4e54582d6178dc21d1ad01c7e057c9343a21a5
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-02-20 15:46:06 +0300

    Task 13 : Create data.sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO EMPLOYEE( ID , FIRST_NAME , LAST_NAME , EMAIL , PHONE) VALUES(1,'Thomas','Fork','thomasfork@example.com','111-222-1111');
+INSERT INTO EMPLOYEE( ID , FIRST_NAME , LAST_NAME , EMAIL , PHONE) VALUES(2,'Janet','Ryan','janetryan@example.com','111-222-2222');
+INSERT INTO EMPLOYEE( ID , FIRST_NAME , LAST_NAME , EMAIL , PHONE) VALUES(3,'Tonny','Megan','tonnymegan@example.com','111-222-3333');
