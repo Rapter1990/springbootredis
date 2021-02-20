b3d3bf29b6dc84a2d0826c287193f2393612ac8a
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-02-20 13:29:44 +0300

    Task 1 : Initial commit and Add Redis Dependency in pom.xml

---
+++ src/test/java/com/springboot/redis/springbootredis/SpringbootredisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.redis.springbootredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootredisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
