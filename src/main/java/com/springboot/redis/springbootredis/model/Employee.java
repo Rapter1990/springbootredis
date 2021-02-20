a18da524947084040149b3a956e9705f67376cc5
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-02-20 15:37:41 +0300

    Task 11 : Create Employee class

---
+++ src/main/java/com/springboot/redis/springbootredis/model/Employee.java
@@ -0,0 +1,22 @@
+package com.springboot.redis.springbootredis.model;
+
+import lombok.Data;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Cache(region = "employeeCache", usage = CacheConcurrencyStrategy.READ_WRITE)
+public class Employee implements Serializable
+{
+    @Id
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+}
